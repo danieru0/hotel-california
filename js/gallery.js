011aa7ab71fcb8a93c0ab62d72cda96e064563ee
Daniel Dąbrowski <elosik123@onet.pl>
2017-11-29 13:20:01 +0100

    javascript refactoring

--- js/gallery.js
+++ js/gallery.js
@@ -1,49 +1,42 @@
 (function () {
     const btn = document.querySelectorAll('.btn-gallery'),
-        number = document.querySelectorAll('.gallery__content-box:not(.hide) > .gallery__number'),
         numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18],
         gallery = document.querySelectorAll('.gallery__content-box');
 
-    document.addEventListener('DOMContentLoaded', function () {
-        changeNumber();
-    });
-
-    function changeNumber() {
-        let boxNotHide = document.querySelectorAll('.gallery__content-box:not(.hide)');
+    function changeImageNumber() {
+        let boxNotHide = document.querySelectorAll('.gallery__content-box:not(.hide) .gallery__number');
         for (let i = 0; i < boxNotHide.length; i++) {
-            number[i].innerHTML = '0' + numbers[i];
+            boxNotHide[i].innerHTML = '0' + numbers[i];
+            if (i > 9) {
+                boxNotHide[i].innerHTML = numbers[i];
+            }
         }
     }
 
+    document.addEventListener('DOMContentLoaded', function () {
+        changeImageNumber();
+    });
+
     for (let i = 0; i < btn.length; i++) {
         btn[i].addEventListener('click', function () {
-            let choice = document.querySelectorAll('.gallery__content-box:not(.' + this.value + ')');
-            document.querySelectorAll('.btn-gallery.active').forEach(function(i) {
+            let selectedButton = document.querySelectorAll('.gallery__content-box:not(.' + this.value + ')');
+            document.querySelectorAll('.btn-gallery.active').forEach(function (i) {
                 i.classList.remove('active');
             });
             this.classList.add('active');
-            if (!(this.value == "all")) {
+            if (!(this.value == 'all')) {
                 document.querySelectorAll('.gallery__content-box').forEach(function (i) {
                     i.classList.remove('hide');
                 });
-                choice.forEach(function (i) {
+                selectedButton.forEach(function (i) {
                     i.classList.add('hide');
                 });
-                let numberHide = document.querySelectorAll('.gallery__content-box:not(.hide) .gallery__number');
-                for (let i = 0; i < numberHide.length; i++) {
-                    numberHide[i].innerHTML = '0' + numbers[i];
-                }
+                changeImageNumber();
             } else {
                 document.querySelectorAll('.gallery__content-box').forEach(function (i) {
                     i.classList.remove('hide');
                 });
-                let numberHide = document.querySelectorAll('.gallery__content-box:not(.hide) .gallery__number');
-                for (let i = 0; i < numberHide.length; i++) {
-                    numberHide[i].innerHTML = '0' + numbers[i];
-                    if (i > 9) {
-                        numberHide[i].innerHTML = numbers[i];
-                    }
-                }
+                changeImageNumber();
             }
         });
     }
